3c8d95a34bfbf7852fda1fc122d8c1c9e08fca31
mmccall0813 <34384161+mmccall0813@users.noreply.github.com>
2021-12-02 13:24:12 -0500

    Update fix.js

--- fix.js
+++ fix.js
@@ -2,5 +2,6 @@ var elems = document.getElementsByTagName("td");
 for(var i = 0; i < elems.length; i++){
 var element = elems[i];
 if(element.innerHTML.includes("<img>")) continue;
+element.style.cursor = "pointer"; // make the cursor look like your hovering over something clickable
 element.onclick = function(){if(this.style.backgroundColor == "rgb(255, 0, 0)"){this.style.backgroundColor = "#FFFFFF";} else this.style.backgroundColor = "#FF0000";};
 }
