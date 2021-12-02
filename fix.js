3c64c87f39edb84688a9ef77bee9aef43160fe47
mmccall0813 <34384161+mmccall0813@users.noreply.github.com>
2021-12-02 13:41:08 -0500

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,7 +1,8 @@
 var elems = document.getElementsByTagName("td");
 for(var i = 0; i < elems.length; i++){
 var element = elems[i];
-if(element.innerHTML.includes("<img>")) continue;
+if(element.innerHTML.includes("<img")) continue; // dont change the headings
+element.style.clipPath = "circle(50% at 50% 50%)"; // make the background shape a circle
 element.style.cursor = "pointer"; // make the cursor look like your hovering over something clickable
 element.onclick = function(){if(this.style.backgroundColor == "rgb(255, 0, 0)"){this.style.backgroundColor = "#FFFFFF";} else this.style.backgroundColor = "#FF0000";};
 }
