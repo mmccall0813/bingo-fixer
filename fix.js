ad127e4df28707108cd68c7990e41ab98624831b
mmccall0813 <34384161+mmccall0813@users.noreply.github.com>
2021-12-02 13:07:18 -0500

    Create fix.js

---
+++ fix.js
@@ -0,0 +1,6 @@
+var elems = document.getElementsByTagName("td");
+for(var i = 0; i < elems.length; i++){
+var element = elems[i];
+if(element.innerHTML.includes("<img>")) continue;
+element.onclick = function(){if(this.style.backgroundColor == "rgb(255, 0, 0)"){this.style.backgroundColor = "#FFFFFF";} else this.style.backgroundColor = "#FF0000";};
+}
